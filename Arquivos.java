e4d7f1d38071cfca0c2410f4308891ccdf2ae0bc
unknown <herlanphs2@gmail.com>
2023-10-27 13:38:13 -0300

    [Teste] Substituição de Array por Lista como coleção de clientes

--- Arquivos.java
+++ Arquivos.java
@@ -1,23 +1,26 @@
 import java.io.BufferedReader;
+import java.util.List;
 
 public class Arquivos {
     
     // Nomes temporarios
     // Gravar Clients
     // OBS: PROCURA UM MEIO DE MELHORA PARA Q ESSAS FUNÇÕES FIQUEM GENERICAS!
-    public static void writter (String filename, Clients Clientes[]){
+    public static void writter (String filename, List<Clients> Clientes){
         try{
             java.io.FileOutputStream arquivo = new java.io.FileOutputStream(filename);
             java.io.PrintWriter pr = new java.io.PrintWriter(arquivo);
             
-            int len = Clientes.length;
+            int len = Clientes.size();
 
             for (int i = 0; i<len; i++){
-                pr.printf("%s - ", Clientes[i].getNome());
-                pr.printf("%s - ", Clientes[i].getTelefone());
-                pr.printf("%s - ", Clientes[i].getCPF());
-                pr.printf("%s", Clientes[i].getEndereco());
+                
+                pr.printf("%s - ", Clientes.get(i).getNome());
+                pr.printf("%s - ", Clientes.get(i).getTelefone());
+                pr.printf("%s - ", Clientes.get(i).getCPF());
+                pr.printf("%s", Clientes.get(i).getEndereco());
                 pr.print("\n");
+                
             }
 
             System.out.println("Atualização do arquivo feita com sucesso");
@@ -31,14 +34,14 @@ public class Arquivos {
         }
     }
 
-    public static void reader(String filename, Clients Clientes[]){
+    public static void reader(String filename, List<Clients> Clientes){
         try{
             java.io.FileInputStream arquivo = new java.io.FileInputStream(filename);
             java.io.InputStreamReader inp = new java.io.InputStreamReader(arquivo);
             java.io.BufferedReader br = new BufferedReader(inp);
 
             String line;
-            int line_id = 0;
+            //int line_id = 0;
             do{
                 line = br.readLine();
                 if (line != null){
@@ -46,7 +49,9 @@ public class Arquivos {
                     // Aqui fica o tratamento para retorna os valores para o Array
                     // Por enquanto vamos usar um slipt
                     String [] dados = line.split(" - ");
-                    Clientes[line_id] = new Clients(dados[0], dados[1], dados[2], dados[3]);
+                    Clients novoclient = new Clients(dados[0], dados[1], dados[2], dados[3]);
+                    Clientes.add(novoclient);
+                    //line_id = line_id + 1;
 
                 }
             }while(line != null);
--- Main.java
+++ Main.java
@@ -1,19 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     
 
     public static void main (String args[]){
         
         // Vamos te que usar coleções aqui
-        Clients Lista_Clientes[] = new Clients[1];
+        List<Clients> Lista_Clientes = new ArrayList<>();
+        //Clients Lista_Clientes[] = new Clients[5];
 
 
         // Os métodos de leitura deveram ser chamados no inicio do programa
         Arquivos.reader("Clientes.txt", Lista_Clientes);
         
         
-        Lista_Clientes[0].Mostra_Client();
+        Lista_Clientes.get(0).Mostra_Client();
 
         // Os métodos de gravação deveram ser chamados no final do programa
         Arquivos.writter("Clientes.txt", Lista_Clientes);
     }
+
+    // Move para cadastro essa função//
+    //public static void Cadastra_Client(Clients Clientes){
+
+
+//    }
 }
